4e73c3268bd17c413fbfaa6adba34cf4e09f4ccd
Rafael Rocha <rocha.rafaelsilva@gmail.com>
2019-12-31 00:02:10 -0300

    Add externs file

--- externs/ieee754-buffer.js
+++ externs/ieee754-buffer.js
@@ -23,30 +23,29 @@
  */
 
 /**
- * @fileoverview rollup configuration file.
- * @see https://github.com/rochars/ieee754-buffer
+ * @fileoverview Externs for uint-buffer 1.0
+ * @see https://github.com/rochars/uint-buffer
+ * @externs
  */
 
-import compiler from '@ampproject/rollup-plugin-closure-compiler';
+/**
+ * A class to write and read unsigned ints to and from byte buffers.
+ */
+var IEEE754Buffer = {};
 
-export default [
-  {
-    input: 'index.js',
-    output: [
-      {
-        file: 'dist/ieee754-buffer.js',
-        name: 'IEEE754Buffer',
-        format: 'umd'
-      },
-    ],
-    plugins: [
-      compiler({
-        language_in: 'ECMASCRIPT6',
-        language_out: 'ECMASCRIPT3',
-        compilation_level: 'SIMPLE',
-        warning_level: 'VERBOSE',
-        externs: ['externs/ieee754-buffer.js']
-      })
-    ]
-  }
-];
+/**
+ * Pack a IEEE 754 floating point number.
+ * @param {!Uint8Array|!Array<number>} buffer The buffer.
+ * @param {number} num The number.
+ * @param {number} index The index to write on the buffer.
+ * @return {number} The next index to write on the buffer.
+ */
+IEEE754Buffer.prototype.pack = function(buffer, num, index=0) {};
+
+/**
+ * Unpack a IEEE 754 floating point number.
+ * @param {!Uint8Array|!Array<number>} buffer The buffer.
+ * @param {number} index The index to read from the buffer.
+ * @return {number} The floating point number.
+ */
+IEEE754Buffer.prototype.unpack = function(buffer, index=0) {};
